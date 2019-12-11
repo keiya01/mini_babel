6c9d22b7b68b413ed8a2c57a6da7fb22845516f9
keiya01 <keiya.s.0210@gmail.com>
2019-12-11 22:39:29 +0900

    Fix to refactor functions and add leave function to visitor of  traverse function

--- src/traverse.ts
+++ src/traverse.ts
@@ -1,36 +1,82 @@
 import estraverse from "estraverse";
-import { Program, VariableDeclaration } from "estree";
+import { Program, VariableDeclaration, VariableDeclarator, Node } from "estree";
 
-const toConstDeclaration = (node: VariableDeclaration) => {
+const getVariableDeclaratorName = (declaration: VariableDeclarator) => {
+	if (declaration.id.type !== "Identifier") {
+		return null;
+	}
+	return declaration.id.name;
+};
+
+const getDeclaratorKind = (node: VariableDeclaration) => {
 	if (node.kind === "var") {
-		node.kind = "const";
+		return "const";
+	}
+	return node.kind;
+};
+
+const toArrowFunction = (declaration: VariableDeclarator) => {
+	if (declaration.init && declaration.init.type === "FunctionExpression") {
+		declaration.init.type = "ArrowFunctionExpression" as any;
+	}
+	return declaration;
+};
+
+const getAssignmentVariableName = (node: Node) => {
+	if (
+		node.type === "ExpressionStatement" &&
+		node.expression.type === "AssignmentExpression" &&
+		node.expression.left.type === "Identifier"
+	) {
+		return node.expression.left.name;
 	}
 };
 
-const toArrowFunction = (node: VariableDeclaration) => {
-	node.declarations = node.declarations.map(declaration => {
-		if (declaration.init && declaration.init.type === "FunctionExpression") {
-			declaration.init.type = "ArrowFunctionExpression" as any;
+function changeVariableDeclarationKind(childNode: VariableDeclaration, declarators: Declarators) {
+	childNode.declarations.map(declaration => {
+		if (declaration.id.type === "Identifier") {
+			childNode.kind = declarators[declaration.id.name];
 		}
-		return declaration;
 	});
-};
+}
+
+type Declarators = { [key: string]: "const" | "let" };
 
 const traverse = (ast: Program) => {
+	const declarators: Declarators = {};
 	estraverse.traverse(ast, {
 		enter: node => {
 			if (node.type !== "Program") {
 				return estraverse.VisitorOption.Skip;
 			}
 
-			ast.body = node.body.map(childNode => {
+			node.body = node.body.map(childNode => {
 				if (childNode.type == "VariableDeclaration") {
-					toConstDeclaration(childNode);
-					toArrowFunction(childNode);
-					return childNode;
+					childNode.declarations = childNode.declarations.map(declaration => {
+						const variable = getVariableDeclaratorName(declaration);
+						if (variable) {
+							declarators[variable] = getDeclaratorKind(childNode);
+						}
+						return toArrowFunction(declaration);
+					});
+				}
+
+				const assignmentVariable = getAssignmentVariableName(childNode);
+				if (assignmentVariable) {
+					declarators[assignmentVariable] = "let";
 				}
 
 				return childNode;
+			});
+		},
+		leave: node => {
+			if (node.type !== "Program") {
+				return estraverse.VisitorOption.Skip;
+			}
+			node.body.map(childNode => {
+				if (childNode.type === "VariableDeclaration") {
+					changeVariableDeclarationKind(childNode, declarators);
+				}
 			});
 		}
 	});
