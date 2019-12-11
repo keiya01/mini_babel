b14da2be193565628408b274723462e11856e6d7
keiya01 <keiya.s.0210@gmail.com>
2019-12-11 10:43:45 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export const say = () => {
-	return 'Hello World';
-};
+import { parse } from "./parse";
+import traverse from "./traverse";
+import unparse from "./unparse";
+
+const filename = process.argv[2];
+let ast = parse(filename);
+ast = traverse(ast);
+const code = unparse(ast);
+
+console.log(code);
