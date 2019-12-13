c98cf164c60307aede9bddff5753678854ab1b8c
keiya01 <keiya.s.0210@gmail.com>
2019-12-13 12:50:44 +0900

    Fix input.js

--- example/input.js
+++ example/input.js
@@ -1,14 +1,37 @@
-var say = function() {
-	return "Hello World";
+/*
+	FizzBuzz Question
+	- Output "Fizz" if multiple of 2
+	- Output "Buzz" if multiple of 5
+	- Output "FizzBuzz" if multiple of 2 and 5
+	- Output number of strings if another number
+*/
+
+var isMultipleOfTwo = function(n) {
+	return n % 2 === 0;
 };
 
-var abc = "abc";
+var isMultipleOfFive = function(n) {
+	return n % 5 === 0;
+};
 
-const t = null;
-t = "test";
+var getFizz = function(n) {
+	return isMultipleOfTwo(n) ? "Fizz" : "";
+};
 
-function hello() {
-	return "Hello";
-}
+var getBuzz = function(n) {
+	return isMultipleOfFive(n) ? "Buzz" : "";
+};
 
-class Hello {}
+var getString = function(n) {
+	return getFizz(n) + getBuzz(n) || n.toString();
+};
+
+var output = function(count) {
+	for (let i = 1; i <= count; i++) {
+		console.log(getString(i));
+	}
+};
+
+// argv is declared for showing the transformation
+let argv = process.argv;
+output(argv[2]);
